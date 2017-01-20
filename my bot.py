276c5315540f3480cabe6ab912c5db7379c20814
STMiki <micou.veillet@gmail.com>
2017-01-21 00:37:46 +0100

    Add files via upload

---
+++ my bot.py
@@ -0,0 +1,45 @@
+import discord
+import asyncio
+from discord.ext import commands
+from random import *
+
+description = ":arrow_forward: Bot par **Miki**\n\nTapez !help pour plus d'aide."
+
+bot = commands.Bot(command_prefix='!', description=description)
+
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+@bot.command()
+async def aide():
+	await bot.say(':arrow_right:  -  !roll : lance un dé\n:arrow_right:  -  !help : affiche l\'aide\n:arrow_right:  -  !janken : pierre feuille ciseaux.\n\n C\'EST TOUT POUR LE MOMENT !!')
+
+@bot.command()
+async def roll():
+    """Rolls a dice in NdN format."""
+    try:
+        await bot.say(':game_die: {} :game_die:'.format(round(random() * 100)))
+    except Exception:
+        await bot.say('Error !')
+        return
+
+@bot.command()
+async def janken():
+	tmp = round(random() * 10)
+	if tmp >= 0 and tmp <= 2:
+		await client.send_message(message.channel, ':scissors:')
+	elif tmp >= 3 and tmp <= 5:
+		await client.send_message(message.channel, ':punch:')
+	else:
+		await client.send_message(message.channel, ':raised_hand:')
+		
+			#await client.send_message(message.channel, ':game_die: {} :game_die:'.format(round(random() * 100)))
+
+
+
+bot.run('MjUxMzExMzQ4NDcyODcyOTYw.CxjrEA.0WJPDeKkVIrNJFImveaRfbU2U4w')
